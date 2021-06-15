fcb1f8a7de16f379c0fc0b6f340373a2907b35e4
nmbrouss <nicolebroussard@Nicoles-MacBook-Air.local>
2021-06-15 15:12:44 -0400

    Created Bubble chart in getPlots function

--- static/app.js
+++ static/app.js
@@ -1,5 +1,5 @@
 function optionChange(id) {
-    barPlot(id);
+    makePlots(id);
     demInfo(id);
 };
 
@@ -18,34 +18,54 @@ function init() {
                 .property("value");
         });
         //Call data and info functions to display on page
-        barPlot(data.names[0]);
+        makePlots(data.names[0]);
         getInfo(data.names[0]);
     });
 }
 init();
 
-function BarPlot(id) {
+function makePlots(id) {
     //read json data
     d3.json("./sample.json").then((data) => {
         console.log(data)
 
         //Get data for values, labels, and hovertext
-        var sampleValues = data.samples_values.slice(0,10).reverse();
-        var otuId = data.otu_ids.slice(0,10).reverse();
+        var sampleValues = data.sample_values.slice(0,10).reverse();
+        var otuId = data.otu_ids.map(d => "OTU " + d).slice(0,10).reverse();
         var otuLabels = data.otu_labels.reverse();
 
-        //Create trace
+        //Create trace1
         var trace1 = {
             text: otuLabels,
             type: "bar",
             x: sampleValues,
             y: otuId,
             orientation: "h"
-        }
+        };
+        //Fix layout
+        var layout = {
+            title: "Top 10 Bacteria Cultures Found"
+        };
+        Plotly.newPlot("bar", trace1, layout);
+
+        //Get values for bubble chart
+        var ids = data.otu_labels;
+        var samValues = data.sample_values;
+        var labels = data.otu_labels;
+
+        //Create trace2
+        var trace2 = {
+            text: labels,
+            x: ids,
+            y: samValues,
+            marker: {
+                color: ids,
+                size: samValues
+            }
+        };
+        var layout = {
+            title: "Bacteria Cultures Per Sample",
+            xlable: "OTU ID"
+        };
     });
-    //Fix layout
-    var layout = {
-        title: "Top 10 Bacteria Cultures Found"
-    }
-    Plotly.newPlot("bar", trace1, layout);
 }
